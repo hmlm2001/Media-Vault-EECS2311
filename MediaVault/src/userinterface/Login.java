d29d4a0cb3ca9fbe17f7391f07a8f3447638cb04
mayowaibi <isaac.ibidun@gmail.com>
2023-02-12 17:22:07 -0500

    Created Login.java using WindowBuilder for LoginUI

--- MediaVault/src/userinterface/Login.java
+++ MediaVault/src/userinterface/Login.java
@@ -1,67 +1,41 @@
 package userinterface;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.EventQueue;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class Login implements ActionListener {
-	
-	private static JLabel usernameLabel;
-	private static JTextField usernameText;
-	private static JLabel passwordLabel;
-	private static JPasswordField passwordText;
-	private static JButton button;
-	private static JLabel success;
-	
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		frame.setSize(900, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(panel);
-	
-		panel.setLayout(null);
-		
-		usernameLabel = new JLabel("Username");
-		usernameLabel.setBounds(10, 20, 80, 25);
-		panel.add(usernameLabel);
-		
-		usernameText = new JTextField(20);
-		usernameText.setBounds(100, 20, 165, 25);
-		panel.add(usernameText);
-		
-		passwordLabel = new JLabel("Password");
-		passwordLabel.setBounds(10, 50, 80, 25);
-		panel.add(passwordLabel);
-		
-		passwordText = new JPasswordField(20);
-		passwordText.setBounds(100, 50, 165, 25);
-		panel.add(passwordText);
-		
-		button = new JButton("Login");
-		button.setBounds(10, 80, 80, 25);
-		button.addActionListener(new Login());
-		panel.add(button);
-		
-		success = new JLabel("");
-		success.setBounds(10, 110, 300, 25);
-		panel.add(success);
-		
-		frame.setVisible(true);
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String username = usernameText.getText();
-		String password = new String(passwordText.getPassword());
-		
-		if (username.equals("USERNAME") && password.equals("PASSWORD")) {
-			success.setText("Login successful!");
-		} else {
-			success.setText("Invalid username/password. Try again.");
-		}
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
 	}
+
 }

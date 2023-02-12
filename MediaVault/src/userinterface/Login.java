a92f60352d44cb66e3461805727312bf80c8d840
mayowaibi <isaac.ibidun@gmail.com>
2023-02-12 00:17:16 -0500

    Created userinterface package with Login class

---
+++ MediaVault/src/userinterface/Login.java
@@ -0,0 +1,18 @@
+package userinterface;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Login {
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setSize(900, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	
+		JPanel panel = new JPanel();
+		
+	}
+}

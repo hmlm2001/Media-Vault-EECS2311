8060e1b0a1696f43e8aa06742b19f69d9585c415
matekoro <korogmaimat@gmail.com>
2023-02-15 13:41:32 -0500

    Moved LoginDB to Persistence Layer

--- MediaVault/src/backend/Encryption.java
+++ MediaVault/src/backend/Encryption.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;  
 import java.security.MessageDigest;  
 import java.security.NoSuchAlgorithmException;  
+import persistence.LoginDB;
 
 public class Encryption {
 	/**

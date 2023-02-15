b2d4b8cc035f4de6cbc8bc8c8962763307af0578
matekoro <korogmaimat@gmail.com>
2023-02-15 12:44:03 -0500

    fixed issue with LoginDB

--- MediaVault/src/backend/Login.java
+++ MediaVault/src/backend/Login.java
@@ -20,11 +20,4 @@ public class Login {
 		return Encryption.encryptVerifyLogin(username, password);
 		
 	}
-	public static void main(String[] args) {
-		System.out.println("Create account Mate with Password 123: " + Login.createAccount("Mate","123"));
-		System.out.println("Login to account Matthew with password 123: " + Login.login("Matthew", "123"));
-		System.out.println("Login to account Mate with password 1234: " + Login.login("Mate", "1234"));
-		System.out.println("Login to account Mate with password 123: " + Login.login("Mate", "123"));
-		
-	}
 }

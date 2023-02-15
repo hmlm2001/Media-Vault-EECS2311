a5214635433ff44252b04382560847d77448506c
matekoro <korogmaimat@gmail.com>
2023-02-15 12:07:58 -0500

    fixed LoginDB

--- MediaVault/src/backend/LoginDB.java
+++ MediaVault/src/backend/LoginDB.java
@@ -39,6 +39,6 @@ public class LoginDB {
 	 */
 	public static boolean verifyLogin(String username, String password) {
 		if(logins.get(username)==password) return true;
-		return true;
+		return false;
 	}
 }

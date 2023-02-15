ec4f4764de737aea561482f97b62e682af237143
mayowaibi <isaac.ibidun@gmail.com>
2023-02-15 14:10:24 -0500

    Created MediaVault.java to run the entire program

---
+++ MediaVault/src/userinterface/MediaVault.java
@@ -0,0 +1,23 @@
+package userinterface;
+
+import java.awt.EventQueue;
+
+public class MediaVault {
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginUI frame = new LoginUI();
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}

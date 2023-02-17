bb88f9a2a5d0902da979238a3b5b710cb3aaac48
Muhammad <ehab.khan01@gmail.com>
2023-02-17 10:40:08 -0500

    Updated the user list, user saves media in an arraylist, that removes or adds media and returns that ArrayList

--- MediaVault/src/backend/MediaCollection.java
+++ MediaVault/src/backend/MediaCollection.java
@@ -4,16 +4,38 @@ import java.util.ArrayList;
 
 public class MediaCollection {
 	
-	public boolean addMedia(Media m) {
-		return false;
+	//make an empty ArrayList to store the user selected media
+	ArrayList<Media> mediaList = new ArrayList<Media>();
+	
+	// this method adds the media into the list
+	public Boolean addMedia(Media m) {
+		//first checks if the user selected media is in the list already
+		if (mediaList.contains(m)) {
+			System.out.println("The selected media is already in your list");
+			return false;
+		}
+		//if not already in the list it then adds the media to the ArrayList
+		else {
+			mediaList.add(m);
+			return true;
+		}
+	}
+	// this method removes the user selected media
+	public Boolean removeMedia(Media m) {
+		//first checks if the media is actually in the ArrayList or not 
+		if (!mediaList.contains(m)) {
+			System.out.println("The selected media is not in your list");
+			return false;
+		}
+		//if it is, then removes the selected user media from the ArrayList
+		else {
+		mediaList.remove(m);
+		return true;
+		}
 		
 	}
 	
-	public boolean removeMedia(Media m) {
-		return false;
-		
-	}
-	
+	// return the changed ArrayList
 	public ArrayList<Media> getMediaList() {
 		return new ArrayList<Media>();
 		

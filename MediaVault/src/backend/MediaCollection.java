4217b4bfde428e8ec68dfffa4ca7006ae0b6da6e
matekoro <korogmaimat@gmail.com>
2023-02-17 11:28:37 -0500

    added MediaCollectionTest and changed some things in MediaCollection

--- MediaVault/src/backend/MediaCollection.java
+++ MediaVault/src/backend/MediaCollection.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 public class MediaCollection {
 	
 	//make an empty ArrayList to store the user selected media
-	ArrayList<Media> mediaList = new ArrayList<Media>();
+	ArrayList<Media> mediaList;
+	
+	public MediaCollection() {
+		mediaList = new ArrayList<Media>();
+	}
 	
 	// this method adds the media into the list
 	public Boolean addMedia(Media m) {
 		//first checks if the user selected media is in the list already
 		if (mediaList.contains(m)) {
-			System.out.println("The selected media is already in your list");
 			return false;
 		}
 		//if not already in the list it then adds the media to the ArrayList
@@ -24,7 +27,6 @@ public class MediaCollection {
 	public Boolean removeMedia(Media m) {
 		//first checks if the media is actually in the ArrayList or not 
 		if (!mediaList.contains(m)) {
-			System.out.println("The selected media is not in your list");
 			return false;
 		}
 		//if it is, then removes the selected user media from the ArrayList
@@ -32,12 +34,6 @@ public class MediaCollection {
 		mediaList.remove(m);
 		return true;
 		}
-		
-	}
-	
-	// return the changed ArrayList
-	public ArrayList<Media> getMediaList() {
-		return new ArrayList<Media>();
 		
 	}
 }

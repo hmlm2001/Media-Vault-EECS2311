bd1bb3b8b7d3a48ebc948f0e89f922d8f7107ca7
Ruth Bezabeh <ruttkas@my.yorku.ca>
2023-03-01 13:26:20 -0500

    re: added new movie class

---
+++ MediaVault/src/backend/Movie.java
@@ -0,0 +1,23 @@
+package backend;
+
+import java.util.Date;
+
+public class Movie extends Media{
+	private int id;
+	private String poster;
+	private String genre;
+	
+	public Movie(String name, Date releaseDate) {
+		super(name, releaseDate);
+	}
+	
+	public static Movie newMovie(String name, Date releaseDate, int id, String poster, String genre) {
+		Movie movie = new Movie(name, releaseDate);
+		movie.id = id;
+		movie.genre = genre;
+		movie.poster = poster;
+		return movie;
+	}
+	
+
+}

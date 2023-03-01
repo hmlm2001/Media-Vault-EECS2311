a376c1720900c6d3b523d44103bdcd68358f7e6f
mayowaibi <isaac.ibidun@gmail.com>
2023-03-01 12:52:09 -0500

    Created JDBC_Connection class to get ResultSet

---
+++ MediaVault/src/persistence/JDBC_Connection.java
@@ -0,0 +1,30 @@
+package persistence;
+
+import java.sql.*;
+
+public class JDBC_Connection {
+	
+	public static ResultSet getResult(String query) {
+		String url = "jdbc:mysql://localhost:3306/mediavault";
+		String user = "admin";
+		String password = "0000";
+
+		try {
+			// create connection
+			Connection con = DriverManager.getConnection(url, user, password);
+			
+			// create statement
+			Statement statement = con.createStatement();
+			
+			// create resultset
+			ResultSet result = statement.executeQuery(query);
+			
+			return result;
+			
+		} catch (SQLException e) { 
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}

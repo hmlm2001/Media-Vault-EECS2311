82146ac8796ef3792ca61f8e329889cc14505801
mayowaibi <isaac.ibidun@gmail.com>
2023-03-02 15:43:23 -0500

    Made changes for testing

--- MediaVault/src/persistence/TMDBTest.java
+++ MediaVault/src/persistence/TMDBTest.java
@@ -15,8 +15,7 @@ public class TMDBTest {
         TmdbMovies movies = new TmdbApi("5068d8984fbaa6b5680cda25bd89f90c").getMovies();
 
         // Getting a list of popular movies
-        List<MovieDb> popularMovies = movies.getPopularMovies("en", 0).getResults();
-//        List<MovieDb> popularMovies2 = movies.getPop;
+//        List<MovieDb> popularMovies = movies.getPopularMovies("en", 0).getResults();
 //        popularMovies.forEach(System.out::println);
 
         // Testing Black Panther
@@ -24,8 +23,17 @@ public class TMDBTest {
         String blackPantherTitle = blackPanther.getTitle();
         String blackPantherReleaseDate = blackPanther.getReleaseDate();
         
-        // Retrieving poster path as a string
-        String blackPantherPosterPath = blackPanther.getImages().get(0).getFilePath();
+        // Retriving overview and runtime
+        String blackPantherOverview = blackPanther.getOverview();
+        int blackPantherRuntime = blackPanther.getRuntime();
+        
+        // Retrieving reviews as strings
+//        List<Reviews> blackPantherReviews = blackPanther.getReviews();
+//        String blackPantherReview1 = blackPanther.getReviews().get(0).getContent();
+//        String blackPantherReview1 = blackPanther.getReviews().get(1).getContent();
+        
+        // Retrieving poster path
+        String blackPantherPosterPath = blackPanther.getPosterPath();
         
         // Retrieving genres as a list of strings
         List<String> blackPantherGenresStrings = new ArrayList<>();
@@ -35,48 +43,12 @@ public class TMDBTest {
         }
         
         // Printing Black Panther movie details
-        System.out.println(blackPanther.getPosterPath());
-        System.out.println(blackPantherTitle);
+        System.out.println("Title: " + blackPantherTitle);
+        System.out.println("Overview: " + blackPantherOverview);
+        System.out.println("Runtime: " + blackPantherRuntime);
         System.out.println("Poster: http://image.tmdb.org/t/p/w500" + blackPantherPosterPath);
         System.out.println("Release Date: " + blackPantherReleaseDate);
-        System.out.println("Genres:");
-        blackPantherGenresStrings.forEach(System.out::println);
-        
-        // Retrieving reviews as strings
-        List<Reviews> blackPantherReviews = blackPanther.getReviews();
-        String blackPantherReview1 = blackPanther.getReviews().get(0).getContent();
-        String blackPantherReview2 = blackPanther.getReviews().get(1).getContent();
-//        System.out.p/rintln(blackPantherReview1);
-//        System.out.println(blackPantherReview2);
-        System.out.println("");
-        
-        // Testing Deadpool
-        MovieDb deadpool = movies.getMovie(293660, "en", images, similar, reviews);
-        String deadpoolTitle = deadpool.getTitle();
-        String deadpoolReleaseDate = deadpool.getReleaseDate();
-        
-        // Retrieving poster path as a string
-        String deadpoolPosterPath = deadpool.getImages().get(0).getFilePath();
-        
-        // Retrieving genres as a list of strings
-        List<String> deadpoolGenresStrings = new ArrayList<>();
-        List<Genre> deadpoolGenres = deadpool.getGenres();
-        for (Genre deadpoolGenre : deadpoolGenres) {
-            deadpoolGenresStrings.add(deadpoolGenre.getName());
-        }
-        
-        // Printing Deadpool movie details
-        System.out.println(deadpoolTitle);
-        System.out.println("Poster: http://image.tmdb.org/t/p/w500" + deadpoolPosterPath);
-        System.out.println("Release Date: " + deadpoolReleaseDate);
-        System.out.println("Genres:");
-        deadpoolGenresStrings.forEach(System.out::println);
-        
-        // Retrieving reviews as strings
-        List<Reviews> deadpoolReviews = deadpool.getReviews();
-        String deadpoolReview1 = deadpool.getReviews().get(0).getContent();
-        String deadpoolReview2 = deadpool.getReviews().get(1).getContent();
-//        System.out.println(deadpoolReview1);
-//        System.out.println(deadpoolReview2);
+        System.out.println("Genres: ");
+        blackPantherGenresStrings.forEach(System.out::println);        
     }
 }

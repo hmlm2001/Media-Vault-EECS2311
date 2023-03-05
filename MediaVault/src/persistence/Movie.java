912b315ba9c72b9b66bd1b6d51b94a369643d511
mayowaibi <isaac.ibidun@gmail.com>
2023-03-04 23:57:45 -0500

    Created MovieDB.java and a new Movie.java
    MovieDB is used to store all the movies in the database. The latter is
    used to store individual movie information.

---
+++ MediaVault/src/persistence/Movie.java
@@ -0,0 +1,105 @@
+package persistence;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Movie {
+	private int id;
+	private String title;
+	private String releaseDate;
+	private String overview;
+	private String posterPath;
+	private String genre;
+	private int runtime;
+	
+	/**
+	 * Constructor for this class only saves the id based on the input argument.
+	 * Getters are used to retrieve other info when needed by sending queries to the DB.
+	 * @param id is the id of the movie in the database
+	 */
+	public Movie(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		ResultSet result;
+		result = JDBC_Connection.getResult("SELECT title FROM allmovies WHERE id='"+id+"';");
+		try {
+			while (result.next()) {
+				this.title = result.getString("title");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return this.title;
+	}
+	
+	public String getReleaseDate() {
+		ResultSet result;
+		result = JDBC_Connection.getResult("SELECT releaseDate FROM allmovies WHERE id='"+id+"';");
+		try {
+			while (result.next()) {
+				this.releaseDate = result.getString("releaseDate");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return this.releaseDate;
+	}
+	
+	public String getOverview() {
+		ResultSet result;
+		result = JDBC_Connection.getResult("SELECT overview FROM allmovies WHERE id='"+id+"';");
+		try {
+			while (result.next()) {
+				this.overview = result.getString("overview");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return this.overview;
+	}
+	
+	public String getPosterPath() {
+		ResultSet result;
+		result = JDBC_Connection.getResult("SELECT posterPath FROM allmovies WHERE id='"+id+"';");
+		try {
+			while (result.next()) {
+				this.posterPath = result.getString("posterPath");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return this.posterPath;
+	}
+	
+	public String getGenre() {
+		ResultSet result;
+		result = JDBC_Connection.getResult("SELECT genre FROM allmovies WHERE id='"+id+"';");
+		try {
+			while (result.next()) {
+				this.genre = result.getString("genre");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return this.genre;
+	}
+	
+	public int getRuntime() {
+		ResultSet result;
+		result = JDBC_Connection.getResult("SELECT runtime FROM allmovies WHERE id='"+id+"';");
+		try {
+			while (result.next()) {
+				this.runtime = result.getInt("runtime");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return this.runtime;
+	}	
+}
--- MediaVault/src/persistence/MovieDB.java
+++ MediaVault/src/persistence/MovieDB.java
@@ -1,44 +1,46 @@
 package persistence;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
-
-import info.movito.themoviedbapi.TmdbApi;
-import info.movito.themoviedbapi.TmdbMovies;
-import info.movito.themoviedbapi.model.MovieDb;
-import static info.movito.themoviedbapi.TmdbMovies.MovieMethod.*;
 
 public class MovieDB {
-	public static void main(String[] args) {
-		TmdbMovies movies = new TmdbApi("5068d8984fbaa6b5680cda25bd89f90c").getMovies();
-		
-		List<MovieDb> allMovies = new ArrayList<>();
-		
-		// Retrieving most popular movies (20 each)
-		List<MovieDb> popularMovies1 = movies.getPopularMovies("en", 1).getResults();
-		List<MovieDb> popularMovies2 = movies.getPopularMovies("en", 2).getResults();
-		List<MovieDb> popularMovies3 = movies.getPopularMovies("en", 3).getResults();
-		List<MovieDb> popularMovies4 = movies.getPopularMovies("en", 4).getResults();
-		List<MovieDb> popularMovies5 = movies.getPopularMovies("en", 5).getResults();
-		
-		// Adding all movies to a single list 
-		allMovies.addAll(popularMovies1);
-		allMovies.addAll(popularMovies2);
-		allMovies.addAll(popularMovies3);
-		allMovies.addAll(popularMovies4);
-		allMovies.addAll(popularMovies5);
-		
-		System.out.println("INSERT INTO allmovies(id,title,releaseDate,overview,posterPath,genre,runtime) VALUES");
-		for (int i = 0; i < allMovies.size(); i++) {
-			int id = allMovies.get(i).getId();
-			MovieDb movie = movies.getMovie(id, "en", similar);
-			String title = movie.getTitle();
-			String releaseDate = movie.getReleaseDate();
-			String overview = movie.getOverview();
-			String posterPath = "http://image.tmdb.org/t/p/w500" + movie.getPosterPath();
-			String genre = movie.getGenres().get(0).getName();
-			int runtime = movie.getRuntime();
-			System.out.println("('" + id + "','" + title + "','" + releaseDate + "','" + overview + "','" + posterPath + "','" + genre + "','" + runtime + "'),");
+	private ArrayList<Movie> movieList;
+	
+	/**
+	 * Constructor for the class initializes the ArrayList then updates it.
+	 */
+	public MovieDB() {
+		movieList = new ArrayList<>();
+		this.updateMovieList();
+	}
+	
+	/**
+	 * Updates the movie list by adding movies using the movie ids in the database.
+	 * Only the id is stored in Java. Other details are retrieved only when necessary.
+	 */
+	private void updateMovieList() {
+		ResultSet result;
+		result = JDBC_Connection.getResult("SELECT id FROM allmovies;");
+		try {
+			while (result.next()) {
+				Movie movie = new Movie(result.getInt("id"));
+				this.movieList.add(movie);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+	}
+		
+	public boolean contains(Movie movie) {
+		return movieList.contains(movie);
+	}
+	
+	public Movie get(int index) {
+		return movieList.get(index);
+	}
+	
+	public int size() {
+		return movieList.size();
 	}
 }

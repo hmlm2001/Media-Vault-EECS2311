7257aacf5eb16fea09f25ff74d70b94e8f8718dc
Ruth Bezabeh <ruttkas@my.yorku.ca>
2023-03-06 20:47:35 -0500

    added watchlist as default vault

--- MediaVault/src/userinterface/MediaCollectionUI.java
+++ MediaVault/src/userinterface/MediaCollectionUI.java
@@ -2,26 +2,38 @@ package userinterface;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Cursor;
 
 import javax.swing.border.EmptyBorder;
+
+
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Font;
+import javax.swing.JScrollPane;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JLayeredPane;
+import java.awt.GridLayout;
 
 @SuppressWarnings("serial")
 public class MediaCollectionUI  extends JFrame{
 	private JPanel contentPane;
 	private Image mv_logo = new ImageIcon(getClass().getResource("/images/logos/mv-logo-white-with-text-no-bg.png")).getImage().getScaledInstance(110, 110, Image.SCALE_SMOOTH);
 	private Image user_icon = new ImageIcon(getClass().getResource("/images/icons/user-icon.png")).getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH);
-
+	private backend.User user;
 	
 	public MediaCollectionUI () {
 		setTitle("MediaVault");
@@ -107,5 +119,69 @@ public class MediaCollectionUI  extends JFrame{
 		logOutButton.setBounds(1185, 17, 88, 29);
 		navbar.add(logOutButton);
 		
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(0, 61, 1280, 600);
+		contentPane.add(layeredPane);
+		
+		JButton WatchListVault = new JButton("   WatchList");
+		WatchListVault.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		layeredPane.setLayer(WatchListVault, 1);
+		WatchListVault.setHorizontalAlignment(SwingConstants.LEADING);
+		WatchListVault.setForeground(new Color(255, 255, 255));
+		WatchListVault.setFont(new Font("Georgia", Font.BOLD, 22));
+		WatchListVault.setBackground(new Color(31, 31, 31));
+		WatchListVault.setBorder(null);
+		WatchListVault.setBounds(0, 0, 1282, 48);
+		layeredPane.add(WatchListVault);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(31, 31, 31));
+		panel.setBounds(0, 37, 1290, 600);
+		layeredPane.add(panel);
+		
+		addMediaButtons(panel);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		
+//		JButton CompletedVault = new JButton("Completed");
+//		CompletedVault.setHorizontalAlignment(SwingConstants.LEFT);
+//		CompletedVault.setForeground(Color.BLACK);
+//		CompletedVault.setFont(new Font("Dialog", Font.BOLD, 22));
+//		CompletedVault.setBackground(Color.LIGHT_GRAY);
+//		CompletedVault.setBounds(0, 200, 1282, 37);
+//		layeredPane.add(CompletedVault);
+//		
+//		JScrollPane scrollPane_1 = new JScrollPane();
+//		scrollPane_1.setBounds(0, 200, 1284, 200);
+//		layeredPane.add(scrollPane_1);
+//		
+//		JButton InProgressVault = new JButton("In progress");
+//		InProgressVault.setHorizontalAlignment(SwingConstants.LEFT);
+//		InProgressVault.setForeground(Color.BLACK);
+//		InProgressVault.setFont(new Font("Dialog", Font.BOLD, 22));
+//		InProgressVault.setBackground(Color.LIGHT_GRAY);
+//		InProgressVault.setBounds(0, 400, 1282, 37);
+//		layeredPane.add(InProgressVault);
+//		
+//		JScrollPane scrollPane_2 = new JScrollPane();
+//		scrollPane_2.setBounds(0, 400, 1284, 200);
+//		layeredPane.add(scrollPane_2);
+		
+	}
+	
+	private void addMediaButtons(JPanel panel ) {
+		ArrayList<backend.Media> mediaCollection = user.getMediaList();
+		//ArrayList<JButton> buttons = new ArrayList<JButton>();
+		JButton mediaButton = null; 
+		for(backend.Media media: mediaCollection)
+			mediaButton = new JButton(media.getName());
+			panel.add(mediaButton);
+			//mediaButton.setIcon(null);
+			//buttons.add();
+		
+		//return null;
 	}
 }

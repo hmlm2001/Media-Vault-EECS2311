2f19282a137c404cfb64f761fa0d180fa2bb6600
mayowaibi <isaac.ibidun@gmail.com>
2023-03-07 13:29:40 -0500

    Searchbar Set Up
    Created helper classes for searchbar intialization. Imported
    miglayout.jar and search image for searchbar layout.

---
+++ MediaVault/src/userinterface/swing/PanelSearch.java
@@ -0,0 +1,78 @@
+package userinterface.swing;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import persistence.Movie;
+
+/**
+ *
+ */
+@SuppressWarnings("serial")
+public class PanelSearch extends JPanel {
+
+    private EventClick event;
+
+    public void addEventClick(EventClick event) {
+        this.event = event;
+    }
+
+    /**
+     * This constructor initializes the PanelSearch and sets the layout using the miglayout package
+     */
+    public PanelSearch() {
+        initComponents();
+        setLayout(new MigLayout("fillx", "0[]0", "0[]0"));
+    }
+
+    /**
+     * Used to set the list of data (movies) to be searched
+     * @param movies - the list of movies to use
+     */
+    public void setMovies(List<Movie> movies) {
+        this.removeAll();
+        for (Movie m : movies) {
+            SearchItem item = new SearchItem(m);
+            //  add event
+            item.addEvent(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    //  when clicked
+                    event.itemClick(m);
+                }
+            });
+            this.add(item, "wrap");
+        }
+        repaint();
+        revalidate();
+        //  Refresh Component
+    }
+
+    /**
+     * @return the number of components
+     */
+    public int getItemSize() {
+        return getComponentCount();
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     */
+    private void initComponents() {
+
+        setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 438, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+    }
+}

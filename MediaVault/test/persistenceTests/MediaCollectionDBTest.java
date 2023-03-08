3da189e5b4611b9d740c27e97c2c45909a511758
Ruth Bezabeh <ruttkas@my.yorku.ca>
2023-03-08 11:46:07 -0500

    added getMediaCollectionId tests for current and new users with out their own collection

--- MediaVault/test/persistenceTests/MediaCollectionDBTest.java
+++ MediaVault/test/persistenceTests/MediaCollectionDBTest.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import backend.Media;
 import backend.UseStub;
+import persistence.JDBC_Connection;
+import persistence.MediaCollectionDB;
 import persistence.MovieDB;
 
 class MediaCollectionDBTest {
@@ -18,10 +22,28 @@ class MediaCollectionDBTest {
 	void init() {
 		UseStub.setStubFlag(false);
 	}
-		
+	/**
+	 * Testing if getMediaCollectionId returns the correct MediaCollection id
+	 * @throws SQLException
+	 */	
 	@Test
 	void getMediaCollectionIdTest() throws SQLException {
+		int userid = 1;
+		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
 		
+		assertEquals(1,collectionid);
+				
+	}
+	/**
+	 * Testing if getMediaCollectionId makes a new MediaCollection and returns the new MediaCollection id for a new user.
+	 * @throws SQLException
+	 */
+	@Test
+	void getMediaCollectionIdTest2() throws SQLException {
+		int userid = 5;
+		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
+		
+		assertEquals(5,collectionid);
 				
 	}
 	

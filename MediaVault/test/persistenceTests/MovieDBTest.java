b57d8da534c7bc1a5659b71ca6eb03794b9f432e
Ruth Bezabeh <ruttkas@my.yorku.ca>
2023-03-08 11:11:46 -0500

    added test cases for non existent users.

--- MediaVault/test/persistenceTests/MovieDBTest.java
+++ MediaVault/test/persistenceTests/MovieDBTest.java
@@ -18,12 +18,25 @@ class MovieDBTest {
 	void init() {
 		UseStub.setStubFlag(false);
 	}
-		
+	/**
+	 * Tests for existing movie
+	 * @throws SQLException
+	 */
 	@Test
 	void getMovieTest() throws SQLException {
 		ResultSet result = MovieDB.getMovie(505642);
 		result.next();
 		assertEquals(505642,result.getInt(1));
+				
+	}
+	/**
+	 * Tests for non-existant movie
+	 * @throws SQLException
+	 */
+	@Test
+	void getMovieTest2() throws SQLException {
+		ResultSet result = MovieDB.getMovie(00000);
+		assertFalse(result.next());
 				
 	}
 
--- MediaVault/test/persistenceTests/UserDBTest.java
+++ MediaVault/test/persistenceTests/UserDBTest.java
@@ -14,12 +14,22 @@ class UserDBTest {
 	void init() {
 		UseStub.setStubFlag(false);
 	}
-		
+	/**
+	 * Tests if getId returns the correct userid.
+	 */
 	@Test
 	void getIdTest() {
 		assertEquals(1,UserDB.getId("user1"));
-		assertEquals(2,UserDB.getId("user2"));
-		
+		assertEquals(2,UserDB.getId("user2"));		
+	}
+	
+	/**
+	 * Tests if getId returns 0 for a non existent user. 
+	 */
+	@Test
+	void getIdTest2() {
+		assertEquals(0,UserDB.getId("user"));
+		assertEquals(0,UserDB.getId("123"));		
 	}
 
 }

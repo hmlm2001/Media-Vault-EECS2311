fe041ce2cd3130b03fcb8ff89255ce7538e61e5d
hmlm2001 <50674910+hmlm2001@users.noreply.github.com>
2023-03-09 18:46:09 -0500

    Some test cases updates
    Removed a redundant "MediaTest.java" and updated
    "MediaCollectionDBTest.java"

--- MediaVault/test/persistenceTests/MediaCollectionDBTest.java
+++ MediaVault/test/persistenceTests/MediaCollectionDBTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import backend.Media;
+import backend.Movie;
 import backend.UseStub;
 import persistence.JDBC_Connection;
 import persistence.MediaCollectionDB;
@@ -47,21 +48,74 @@ class MediaCollectionDBTest {
 				
 	}
 	
+	/**
+	 * Testing if the arraylist returned has all the corresponding movies to the collectionID in the database
+	 * @throws SQLException
+	 */
 	@Test
-	void getMediaCollectionTest() throws SQLException {
-		
-				
+	void getMediaCollectionTest1() throws SQLException {
+		int userid = 1;
+		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
+		ArrayList<Media> collectionDB = MediaCollectionDB.getMediaCollection(collectionid);
+		//TODO Make sure getMediaCollectionTest movie ids are up to date
+		assertTrue(collectionDB.contains(new Movie(631842)));
+		assertTrue(collectionDB.contains(new Movie(505642)));
+	}
+	/**
+	 * Testing if the addMediaCollection adds media successfully
+	 * @throws SQLException
+	 */
+	@Test
+	void addMediaCollectionTest1() throws SQLException {
+		//TODO Test addMediaCollectionTest
+		int userid = 1;
+		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
+		Movie movie = new Movie(411);
+		ArrayList<Media> collection = MediaCollectionDB.getMediaCollection(collectionid);
+		assertFalse(collection.contains(movie));
+		assertTrue(MediaCollectionDB.addMediaCollection(collectionid, 411));
+		collection = MediaCollectionDB.getMediaCollection(collectionid);
+		assertTrue(collection.contains(movie));
+	}
+	
+	/**
+	 * Testing when a movie is added to a collection containing that movie
+	 * @throws SQLException
+	 */
+	@Test
+	void addMediaCollectionTest2() throws SQLException{
+		int userid = 1;
+		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
+		Movie movie = new Movie(411);
+		ArrayList<Media> collection = MediaCollectionDB.getMediaCollection(collectionid);
+		assertTrue(collection.contains(movie));
+		assertFalse(MediaCollectionDB.addMediaCollection(collectionid, 411));
+	}
+	
+	/**
+	 * Testing if the removeMediaCollection removes media successfully
+	 * @throws SQLException
+	 */
+	@Test
+	void removeMediaCollectionTest1() throws SQLException {
+		//TODO Test removeMediaCollectionTest
+		int userid = 1;
+		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
+		Movie movie = new Movie(411);
+		ArrayList<Media> collection = MediaCollectionDB.getMediaCollection(collectionid);
+		assertTrue(collection.contains(movie));
+		assertTrue(MediaCollectionDB.addMediaCollection(collectionid, 411));
+		collection = MediaCollectionDB.getMediaCollection(collectionid);
+		assertFalse(collection.contains(movie));
 	}
 	
 	@Test
-	void addMediaCollectionTest() throws SQLException {
-		
-				
-	}
-	
-	@Test
-	void removeMediaCollectionTest() throws SQLException {
-		
-				
+	void removeMediaCollectionTest2() throws SQLException {
+		int userid = 1;
+		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
+		Movie movie = new Movie(411);
+		ArrayList<Media> collection = MediaCollectionDB.getMediaCollection(collectionid);
+		assertFalse(collection.contains(movie));
+		assertFalse(MediaCollectionDB.addMediaCollection(collectionid, 411));
 	}
 }

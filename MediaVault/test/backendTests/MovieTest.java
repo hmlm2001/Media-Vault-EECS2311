b2c646b367eb10f7acf8aad95248ae6ff77da78b
Ruth Bezabeh <ruttkas@my.yorku.ca>
2023-03-10 03:08:38 -0500

    modified tests and added comments

--- MediaVault/test/backendTests/MovieTest.java
+++ MediaVault/test/backendTests/MovieTest.java
@@ -20,61 +20,92 @@ class MovieTest {
 	Movie movie2;
 	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 	
+	/**
+	 * sets the StubFlag to true in the beginning so the tests are run on the stub database.
+	 * And creates the movie objects
+	 */
 	@BeforeEach
 	void init() throws ParseException {
 		UseStub.setStubFlag(true);
 		movie1 = new Movie(505642);
-		//Media movie2 = new Movie(0);// invalid id
+		movie2 = new Movie(0);
 	}
 	
+	/**
+	 * tests if getId returns the correct Id for the movie.
+	 */
 	@Test
 	void getIdTest() {
 		assertEquals(505642, movie1.getId());
 	}
 	
+	/**
+	 * tests if getTitle returns the correct Title for the movie.
+	 */
 	@Test
 	void getTitleTest() {
 		assertEquals("Black Panther: Wakanda Forever", movie1.getTitle());
-	}	
+	}
 	
+	/**
+	 * tests if getOverview returns the correct Overview for the movie.
+	 */
 	@Test
 	void getOverviewTest() {
 		assertEquals("Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death.  As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.", movie1.getOverview());
 	}
 	
+	/**
+	 * tests if getPosterPath returns the correct PosterPath for the movie.
+	 */	
 	@Test
 	void getPosterPathTest() {
 		assertEquals("http://image.tmdb.org/t/p/w500/sv1xJUazXeYqALzczSZ3O6nkH75.jpg", movie1.getPosterPath());
 	}
 	
+	/**
+	 * tests if getGenre returns the correct Genre for the movie.
+	 */
 	@Test
 	void getGenreTest() {
 		assertEquals("Action", movie1.getGenre());
 	}
+	/**
+	 * tests if getRuntime returns the correct runtime for the movie.
+	 */
+	
 	@Test
 	void getRuntimeTest() {
 		assertEquals(162, movie1.getRuntime());
 	}
-//	@Test	
-//	void getTitleTest2() {
-//		assertEquals("Movie2", movie2.getName());
-//	}	
+	/**
+	 * tests if getReleaseDate returns the correct date for the movie.
+	 * @throws ParseException
+	 */
 	
 	@Test
 	void getReleaseDateTest() throws ParseException {
-		assertEquals(Date.valueOf("2022-11-09"), movie1.getReleaseDate());
+		assertEquals("2022-11-09", movie1.getReleaseDate());
 	}
-	
+	/**
+	 * tests for a new movie that's not one of the valid ids in the stub.
+	 * defaults to Puss in Boots: The Last Wish, ONLY FOR TESTING.
+	 */
+	@Test	
+	void getTitleTest2() {
+		assertEquals("Puss in Boots: The Last Wish", movie2.getTitle());
+	}
+	/**
+	 * tests for a new movie that's not one of the valid ids in the stub.
+	 * should return the release date for Puss in Boots: The Last Wish.
+	 */
 	@Test
 	void getReleaseDateTest2() throws ParseException {
-		assertEquals(dateFormat.parse("2022-11-09"), movie1.getReleaseDate());
+		assertEquals("2022-12-07", movie2.getReleaseDate());
 	}
-	
-//	@Test
-//	void getReleaseDateTest2() throws ParseException {
-//		assertEquals(dateFormat.parse("2021-05-02"), movie2.getReleaseDate());
-//	}
-//	
+	/**
+	 * sets the StubFlag to false so testing does not interfere with the normal operation of the project.
+	 */
 	@AfterAll
 	static void cleanup() {
 		UseStub.setStubFlag(false);
--- MediaVault/test/persistenceTests/MediaCollectionDBTest.java
+++ MediaVault/test/persistenceTests/MediaCollectionDBTest.java
@@ -7,7 +7,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+//import org.junit.jupiter.api.ClassOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import backend.Media;
 import backend.Movie;
@@ -16,6 +20,7 @@ import persistence.JDBC_Connection;
 import persistence.MediaCollectionDB;
 import persistence.MovieDB;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MediaCollectionDBTest {
 
 
@@ -28,7 +33,8 @@ class MediaCollectionDBTest {
 	 * @throws SQLException
 	 */	
 	@Test
-	void getMediaCollectionIdTest() throws SQLException {
+	@Order(1)
+	void getMediaCollectionIdTest1() throws SQLException {
 		int userid = 1;
 		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
 		
@@ -40,6 +46,7 @@ class MediaCollectionDBTest {
 	 * @throws SQLException
 	 */
 	@Test
+	@Order(2)
 	void getMediaCollectionIdTest2() throws SQLException {
 		int userid = 5;
 		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
@@ -53,29 +60,44 @@ class MediaCollectionDBTest {
 	 * @throws SQLException
 	 */
 	@Test
+	@Order(3)
 	void getMediaCollectionTest1() throws SQLException {
 		int userid = 1;
 		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
 		ArrayList<Media> collectionDB = MediaCollectionDB.getMediaCollection(collectionid);
 		//TODO Make sure getMediaCollectionTest movie ids are up to date
-		assertTrue(collectionDB.contains(new Movie(631842)));
-		assertTrue(collectionDB.contains(new Movie(505642)));
+		assertEquals(631842,collectionDB.get(0).getId());
+		assertEquals(505642,collectionDB.get(1).getId());
+		assertEquals(2, collectionDB.size());
 	}
+	
 	/**
 	 * Testing if the addMediaCollection adds media successfully
 	 * @throws SQLException
 	 */
 	@Test
+	@Order(4)
 	void addMediaCollectionTest1() throws SQLException {
 		//TODO Test addMediaCollectionTest
 		int userid = 1;
 		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
 		Movie movie = new Movie(411);
 		ArrayList<Media> collection = MediaCollectionDB.getMediaCollection(collectionid);
-		assertFalse(collection.contains(movie));
+		
+		for(Media m: collection) {
+			assertFalse(m.getId() == 411);			
+		}
+		
 		assertTrue(MediaCollectionDB.addMediaCollection(collectionid, 411));
+		
+		
+		int count = 0;
 		collection = MediaCollectionDB.getMediaCollection(collectionid);
-		assertTrue(collection.contains(movie));
+		for(Media m: collection) {
+			if(m.getId() == 411)
+				count++;
+			}
+		assertEquals(1,count);
 	}
 	
 	/**
@@ -83,43 +105,68 @@ class MediaCollectionDBTest {
 	 * @throws SQLException
 	 */
 	@Test
+	@Order(5)
 	void addMediaCollectionTest2() throws SQLException{
 		int userid = 1;
 		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
 		Movie movie = new Movie(411);
 		ArrayList<Media> collection = MediaCollectionDB.getMediaCollection(collectionid);
-		assertTrue(collection.contains(movie));
+		int count = 0;
+		for(Media m: collection) {
+			if(m.getId() == 411)
+				count++;
+			}
+		assertEquals(1,count);
 		assertFalse(MediaCollectionDB.addMediaCollection(collectionid, 411));
 	}
-	
+
 	/**
 	 * Testing if the removeMediaCollection removes media successfully
 	 * @throws SQLException
 	 */
 	@Test
+	@Order(6)
 	void removeMediaCollectionTest1() throws SQLException {
 		//TODO Test removeMediaCollectionTest
 		int userid = 1;
 		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
 		Movie movie = new Movie(411);
 		ArrayList<Media> collection = MediaCollectionDB.getMediaCollection(collectionid);
-		assertTrue(collection.contains(movie));
-		assertTrue(MediaCollectionDB.addMediaCollection(collectionid, 411));
+		int count = 0;
 		collection = MediaCollectionDB.getMediaCollection(collectionid);
-		assertFalse(collection.contains(movie));
+		for(Media m: collection) {
+			if(m.getId() == 411)
+				count++;
+			}
+		assertEquals(1, count);
+		
+		assertTrue(MediaCollectionDB.removeMediaCollection(collectionid, 411));
+		collection = MediaCollectionDB.getMediaCollection(collectionid);
+		
+		for(Media m: collection) {
+			assertFalse(m.getId() == 411);			
+		}
 	}
+	
+	
+	
+	
 
 	/**
 	 * Testing when the removeMediaCollection removes media from a collection not containing the media initially
 	 * @throws SQLException
 	 */
 	@Test
+	@Order(7)
 	void removeMediaCollectionTest2() throws SQLException {
 		int userid = 1;
 		int collectionid = MediaCollectionDB.getMediaCollectionId(userid);
 		Movie movie = new Movie(411);
 		ArrayList<Media> collection = MediaCollectionDB.getMediaCollection(collectionid);
-		assertFalse(collection.contains(movie));
-		assertFalse(MediaCollectionDB.addMediaCollection(collectionid, 411));
+		for(Media m: collection) {
+			assertFalse(m.getId() == 411);			
+		}
+		assertFalse(MediaCollectionDB.removeMediaCollection(collectionid, 411));
 	}
+
 }

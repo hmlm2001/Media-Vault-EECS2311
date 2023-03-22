7d46bfab7701d06a4659a99650f7a3ca149e7198
mayowaibi <isaac.ibidun@gmail.com>
2023-03-22 11:52:26 -0400

    Minor fix

--- MediaVault/src/userinterface/swing/SearchbarLogoSetup.java
+++ MediaVault/src/userinterface/swing/SearchbarLogoSetup.java
@@ -14,6 +14,7 @@ import javax.swing.JPopupMenu;
 import javax.swing.SwingConstants;
 
 import backend.Movie;
+import backend.User;
 import persistence.AllMoviesDB;
 import userinterface.MoviePageUI;
 

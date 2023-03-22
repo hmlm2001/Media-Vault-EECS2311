4e806e576bb8431687c0012820a5e585e2dc1358
matekoro <korogmaimat@gmail.com>
2023-03-22 11:44:52 -0400

    final changes to sql database script

--- MediaVault/database/SETUP.sql
+++ MediaVault/database/SETUP.sql
@@ -50,7 +50,6 @@ Create table review (
 );
 
 
-
 INSERT INTO users(username) VALUES
 ('user1'),('user2'),('user3'),('user4'),('user5'),('user6'),('user7'),('user8'),
 ('user9'),('user10'),('user11');

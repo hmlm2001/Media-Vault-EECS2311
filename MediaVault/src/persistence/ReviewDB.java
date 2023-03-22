da9b343bf4860a5656422508721b163bb6e3ef69
matekoro <korogmaimat@gmail.com>
2023-03-22 15:07:44 -0400

    added comments, complete

--- MediaVault/src/persistence/ReviewDB.java
+++ MediaVault/src/persistence/ReviewDB.java
@@ -9,6 +9,11 @@ import backend.Review;
 import backend.User;
 
 public class ReviewDB {
+	/**
+	 * gets a list of reviews associated with a media
+	 * @param media is the media under review
+	 * @return the array list of media containing 3 reviews
+	 */
 	public static ArrayList<Review> get(Media media){
 		ArrayList<Review> reviews = new ArrayList<Review>();
 		ResultSet result;
@@ -23,6 +28,12 @@ public class ReviewDB {
 		}
 		return reviews;
 	}
+	/**
+	 * checks if a user already has a review in the db for the related media
+	 * @param movieId is the id of the movie that is getting reviewed
+	 * @param username is the username of the user giving the review
+	 * @return true if it exists, false otherwise
+	 */
 	public static boolean userReviewed(int movieId, String username) {
 		ResultSet result;
 		result = JDBC_Connection.getResult("SELECT * FROM review WHERE username='"+username+"'AND movieid="+movieId+";");
@@ -31,12 +42,16 @@ public class ReviewDB {
 			      return false;
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return true;
 	}
-	
+	/**
+	 * add a media to the db, overwrite if it exists, insert if it doesnt
+	 * @param movieId is the id of the movie being reviewed
+	 * @param reviewObj the isntnace of the review
+	 * @return true if successful, false otherwise
+	 */
 	public static boolean add(int movieId, Review reviewObj) {
 		String username = reviewObj.getUser().getUsername();
 		String review = reviewObj.getReview();
@@ -46,7 +61,5 @@ public class ReviewDB {
 			JDBC_Connection.execute("REPLACE INTO review VALUES ('"+username+"',"+movieId+",'"+review+"');");
 		}
 		return true;
-		
-		
 	}
 }

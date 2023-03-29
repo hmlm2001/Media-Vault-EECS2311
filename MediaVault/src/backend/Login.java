8f55d3095fa67e24119e2b046a684b4e30e6a4a7
matekoro <korogmaimat@gmail.com>
2023-03-29 11:34:22 -0400

    Fixed issue with lowercase/uppercase login

--- MediaVault/src/backend/Login.java
+++ MediaVault/src/backend/Login.java
@@ -8,7 +8,7 @@ public class Login {
 	 * @return true if new account has been created, false otherwise
 	 */
 	public static boolean createAccount(String username, String password) {
-		return Encryption.encryptNewAccount(username, password);
+		return Encryption.encryptNewAccount(username.toLowerCase(), password);
 	}
 	/**
 	 * Logs into an already existing account
@@ -17,6 +17,6 @@ public class Login {
 	 * @return true if login is successful, false otherwise
 	 */
 	public static boolean login(String username, String password) {
-		return Encryption.encryptVerifyLogin(username, password);
+		return Encryption.encryptVerifyLogin(username.toLowerCase(), password);
 	}
 }
